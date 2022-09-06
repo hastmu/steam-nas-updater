9f37f15a31d07899d02643e07072c307697e2865
hastmu <gh-hastmu@gmx.de>
2022-09-06 21:48:43 +0200

    first work

--- steam-nas-updater.sh
+++ steam-nas-updater.sh
@@ -2,12 +2,38 @@
 
 echo "Status: Work in Progress - no implemented function"
 
+declare -A BINS
+declare -a bin=( "steamcmd" "acf_to_json" "ls" )
+
+function install() {
+   # dependencies: steamcmd, acf_to_json
+   # return 0 if all fine otherwise install or return non-zero
+   
+   # check if we are good to go...
+   local item=""
+   local ret=0
+   for item in ${bin[@]}
+   do
+      BINS[${item}]="$(which ${item})"
+      [ -z "${BINS[${item}]}" ] && ret=1
+   done
+   
+   # if not try to install
+   if [ $ret -ne 0 ]
+   then
+      echo "try install..."
+   fi
+   
+   # feedback final status
+   return ${ret}
+}
+
 case "$1"
 in
 
 install) {
    echo "this will install all needed dependencies"
-
+   install
 } ;;
 
 setup) {
@@ -17,7 +43,14 @@ setup) {
 
 update-lib) {
    echo "this will run the update."
-
+   if install
+   then
+      echo "install ok"
+      declare -p BINS
+   else
+      echo "install broken"
+      declare -p BINS
+   fi
 } ;;
 
 *) {
